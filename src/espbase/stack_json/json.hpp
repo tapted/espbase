3936db8a5898f1bb847b3d9ca8540e584d3e2b6f
Trent Apted <tapted@gmail.com>
2026-08-22 22:35:26 +1000

    Add parser to monolith

--- src/espbase/stack_json/json.hpp
+++ src/espbase/stack_json/json.hpp
@@ -2,11 +2,14 @@
 
 #include "espbase/stack_json/builder.hpp"
 #include "espbase/stack_json/document.hpp"
+#include "espbase/stack_json/parser.hpp"
 
+using sjson::bind;
+using sjson::Builder;
+using sjson::json_parser;
 using sjson::node;
 using sjson::node_if;
 using sjson::path;
-using sjson::stack_json;
-using sjson::stack_array;
 using sjson::span_array;
-using sjson::Builder;
+using sjson::stack_array;
+using sjson::stack_json;
