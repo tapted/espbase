4eeaf882d578b5ebae22d23ab5fb61406e393ed4
Trent Apted <tapted@gmail.com>
2026-09-13 09:59:28 +1000

    Switch to gzip for network logger. Make it a proper esp-idf component.

--- src/espbase/boot/network_logger.cpp
+++ src/espbase/boot/network_logger.cpp
@@ -7,8 +7,8 @@
 
 #include "espbase/circular_history_buffer.hpp"
 
-extern const uint8_t spa_html_br_start[] asm("_binary_network_logger_html_br_start");
-extern const uint8_t spa_html_br_end[]   asm("_binary_network_logger_html_br_end");
+extern const uint8_t spa_html_gz_start[] asm("_binary_network_logger_html_gz_start");
+extern const uint8_t spa_html_gz_end[]   asm("_binary_network_logger_html_gz_end");
 
 static vprintf_like_t original_vprintf_ = nullptr;
 static constinit CircularHistoryBuffer buffer_;
@@ -57,10 +57,10 @@ static esp_err_t stream_log_handler(httpd_req_t* req) {
 
 static esp_err_t index_handler(httpd_req_t* req) {
   httpd_resp_set_type(req, "text/html");
-  httpd_resp_set_hdr(req, "Content-Encoding", "br");
+  httpd_resp_set_hdr(req, "Content-Encoding", "gzip");
   
-  size_t len = spa_html_br_end - spa_html_br_start;
-  return httpd_resp_send(req, reinterpret_cast<const char*>(spa_html_br_start), len);
+  size_t len = spa_html_gz_end - spa_html_gz_start;
+  return httpd_resp_send(req, reinterpret_cast<const char*>(spa_html_gz_start), len);
 }
 
 void initialize_network_logger(size_t size_bytes, bool use_psram) {
